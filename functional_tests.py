28b4ee015892f4636546ed056ad95955337f369c
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-12 19:29:15 +0530

    Modified the functional test greatly. It now uses several methods provided by the Selenium browser object. Check test docs for more info.

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@ Created on Tue Aug 12 10:47:08 2014
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import os
 import pytest
 
@@ -38,11 +39,31 @@ def browser(driver_init):
     
     return b
 
-#@pytest.mark.xfail(reason="To-Do not in title yet")
+@pytest.mark.xfail(reason="Need to add the requisite features")
 def test_start_list_retrieve_later(browser):
-    # Obtain missile command from Firefox silos
+    """
+    This test does some stuff in the browser. We use some methods provided by
+    the Selenium browser object. We test things like the text within <H1>. We
+    obtain a reference to an inputbox by its ID, and check that its 'placeholder'
+    attribute is as we set it. We also send text to the inputbox.
+    Lastly, we check that a table's tr element has our required text in any of
+    its rows.
+    """
     browser.get('http://localhost:8000')
     
-    # Check title 
     #assert 'Django' in browser.title
     assert 'To-Do' in browser.title, "Browser title was " + browser.title
+    header_text = browser.find_element_by_tag_name('h1').text
+    assert 'To-Do' in header_text
+    
+    inputbox = broswer.find_element_by_id('id_new_item')
+    assert inputbox.get_attribute('placeholder') == 'Enter a to-do item'
+    
+    inputbox.send_keys('Buy peacock feathers')
+    
+    inputbox.send_keys(Keys.ENTER)
+    
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    
+    assert any(row.text == '1: Buy peacock feathers' for row in rows)
