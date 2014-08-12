b5f91106eab8f6572383e7d2a94540dc4d2218d1
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-12 11:07:18 +0530

    Changed the browser title test up a bit. Added some comments.

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,12 @@ Created on Tue Aug 12 10:47:08 2014
 
 from selenium import webdriver
 
+# Initiate Selenium blast sequence with Firefox missiles
 browser = webdriver.Firefox()
+
+# Obtain missile command from Firefox silos
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Check title 
+#assert 'Django' in browser.title
+assert 'To-Do' in browser.title
