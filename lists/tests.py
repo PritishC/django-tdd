371354b9905d5aeb3f2465213600c5a5933be0c2
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-12 19:03:29 +0530

    Added a test for home_page() view function. Now to change the view function itself.

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,7 @@ Created on Tue Aug 12 12:38:42 2014
 """
 
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from lists.views import home_page
 import pytest
 
@@ -16,3 +17,16 @@ def test_root_url_resolving_to_home_page():
     
     # Test that resolve when called with '/' finds a function called home_page.
     assert found.func == home_page
+    
+def test_home_page_return_correct_html():
+    """
+    This test creates a HttpRequest object, and obtains the HttpResponse when
+    applied to the home_page() view function.
+    The content attribute of the HttpResponse object is tested to see that it
+    has certain bits of HTML. The b'' syntax is because content is in bytes.
+    """
+    request = HttpRequest()
+    response = home_page(request)
+    assert response.content.startswith(b'<html>')
+    assert '<title>To-Do lists</title>' in response.content
+    assert response.content.endswith(b'</html>')
