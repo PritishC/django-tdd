b94775a60de60c3dd8dcca92c1962e03cf71dd78
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-17 18:12:22 +0530

    Modified the project settings in superlists/settings.py. Attempt to use a MySQL database on localhost. Already set up the DB by following Recipe 1.1 in MySQL cookbook, 2nd ed.

--- superlists/settings.py
+++ superlists/settings.py
@@ -59,8 +59,14 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'pritishc',
+        'PASSWORD': 'cr@p123',
+        'HOST': 'localhost',
+        'PORT': '5000',
     }
 }
 
