63b5ae013ba6faa69782dac15aa4226a4c0780be
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-17 20:21:18 +0530

    Modified the home_page view function back to if-else POST blocks. objects.create is a shorthand to create objects and save automatically.

--- lists/views.py
+++ lists/views.py
@@ -4,9 +4,11 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(httprequest):
-    item = Item()
-    item.text = httprequest.POST.get('item_text', '')
-    item.save()
+    if httprequest.method == 'POST':
+        new_item_text = httprequest.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
 
     return render(httprequest, 'home.html',
-        {'new_item_text': item.text,})
+        {'new_item_text': new_item_text,})
