ffd3c2cea86df4c0cc7a41c061a9e73f2ddf9ad5
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-17 10:05:53 +0530

    Commented out time.sleep.

--- functional_tests.py
+++ functional_tests.py
@@ -63,7 +63,7 @@ def test_start_list_retrieve_later(browser):
     
     inputbox.send_keys(Keys.ENTER)
     import time
-    time.sleep(10) # shows us that the CSRF mechanism is not enabled    
+    #time.sleep(10) # shows us that the CSRF mechanism is not enabled
     
     table = browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
