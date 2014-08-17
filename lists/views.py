ca8d29e0a8ac4e65c301b121eebefb7c2a5d9d74
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-17 13:09:22 +0530

    Added a simple if-block to deal with POST for now. This will be changed soon.

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(httprequest):
+    if httprequest.method == 'POST':
+        return HttpResponse(httprequest.POST['item_text'])
     return render(httprequest, 'home.html')
