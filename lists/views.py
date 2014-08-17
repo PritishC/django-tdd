9b885a885a611a80bde3a5be07e903d113b61237
VaticanCameos <chakrabortypritish@gmail.com>
2014-08-17 20:09:57 +0530

    Modified the view home_page function. This means any test which tries to render the home template will need the django_db mark. Saved an Item in the DB.

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(httprequest):
+    item = Item()
+    item.text = httprequest.POST.get('item_text', '')
+    item.save()
+    
     return render(httprequest, 'home.html',
         {'new_item_text': httprequest.POST.get('item_text', ''),})
